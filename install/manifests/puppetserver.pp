b369eb6a51cca66882a74837e3ac79b6b0a45cb7
Mihai Preda <mpreda@outlook.com>
2024-01-21 13:08:15 -0800

    removed sysconfig file

--- install/manifests/puppetserver.pp
+++ install/manifests/puppetserver.pp
@@ -2,13 +2,6 @@ package { 'puppetserver':
   ensure => installed,
 }
 
-# # file { 'sysconfig-puppetserver':
-# #   ensure  => file,
-# #   path    => '/etc/sysconfig/puppetserver',
-# #   source  => '/vagrant/install/files/sysconfig-puppetserver',
-# #   require => Package['puppetserver'],
-# }
-
 service { 'puppetserver':
   ensure  => true,
   enable  => true,
