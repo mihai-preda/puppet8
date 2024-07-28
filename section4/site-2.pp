a4d6da6ecb86d9c393fa0f3c88ab40a365165abf
Mihai Preda <mpreda@outlook.com>
2024-07-28 13:15:42 -0700

    renamed epele release to match EL and not Oracle

--- section4/site-2.pp
+++ section4/site-2.pp
@@ -2,13 +2,13 @@
 # fact host name is not present in PP8. It's actually Host (stdlib)
 concat { '/etc/motd': }
 
-package { 'oracle-epel-release-el8':
+package { 'epel-release':
   ensure => 'installed',
 }
 
 package { 'figlet':
   ensure  => 'installed',
-  require => Package['oracle-epel-release-el8'],
+  require => Package['epel-release'],
 }
 
 exec { 'motd.hostname':
